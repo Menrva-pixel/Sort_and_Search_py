fe44340ca627dc84989d37203083155284249f63
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-07-05 17:45:13 +0700

    Perbaikan pilihan
    memperbaiki opsi pilihan

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 # main.py
-from option import ui
-from option import nonui
+import os
 
 def run_with_ui():
-    app = ui.App()
-    app.mainloop()
+    os.system("python option/ui.py")
 
 def run_without_ui():
-    nonui.run_app()
+    os.system("python option/nonui.py")
 
 def main():
     print("Pilih opsi:")
     print("1. Jalankan dengan antarmuka pengguna (UI)")
     print("2. Jalankan tanpa antarmuka pengguna (Non-UI)")
+    print("3. Keluar Aplikasi")
 
-    choice = input("Masukkan pilihan (1/2): ")
+    choice = input("Masukkan pilihan (1/2/3): ")
 
     if choice == "1":
         run_with_ui()
     elif choice == "2":
         run_without_ui()
+    elif choice == "3":
+        quit
     else:
         print("Pilihan tidak valid.")
 
