efe798088a24aa6754209283bdfe97a91903dc15
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-07-05 17:57:51 +0700

    looping update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 import os
+from art import *
 
 def run_with_ui():
     os.system("python option/ui.py")
@@ -7,22 +8,32 @@ def run_with_ui():
 def run_without_ui():
     os.system("python option/nonui.py")
 
+   
+tprint("APLIKASI")
+ 
+
+
 def main():
     print("Pilih opsi:")
     print("1. Jalankan dengan antarmuka pengguna (UI)")
     print("2. Jalankan tanpa antarmuka pengguna (Non-UI)")
     print("3. Keluar Aplikasi")
 
-    choice = input("Masukkan pilihan (1/2/3): ")
 
-    if choice == "1":
-        run_with_ui()
-    elif choice == "2":
-        run_without_ui()
-    elif choice == "3":
-        quit
-    else:
-        print("Pilihan tidak valid.")
+    while True:
+        choice = input("Masukkan pilihan (1/2/3): ")
+
+        if choice == "1":
+            run_with_ui()
+            break
+        elif choice == "2":
+            run_without_ui()
+            break
+        elif choice == "3":
+            print("Aplikasi selesai.")
+            break
+        else:
+            print("Pilihan tidak valid.")
 
 if __name__ == "__main__":
     main()
